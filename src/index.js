f3f44dfd9d7d279636646ad831bc377384c8d904
Timur Musin <tmdevwork@gmail.com>
2021-05-14 21:15:44 +0300

    Оптимизация

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-import Post from "./Post"
+import * as $ from "jquery";
+import Post from "@models/Post"
 import "./styles/styles.css"
 import json from "./assets/json.json";
-import WebpackLogo from "./assets/webpack-logo.png";
+import WebpackLogo from "./assets/webpack-logo";
 import xml from "./assets/data.xml";
 import csv from "./assets/data.csv";
+
 const post = new Post('Webpack Post Title',WebpackLogo);
 
-console.log('Post to String date:', post.toString());
-console.log('JSON:',json);
-console.log('XML:',xml);
-console.log('CSV:',csv);
+$("pre").html(post.toString());
+
+// console.log('Post to String date:', post.toString());
+// console.log('JSON:',json);
+// console.log('XML:',xml);
+// console.log('CSV:',csv);
